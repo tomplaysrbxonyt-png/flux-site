2816280c3b7659f77a60906a4f079fe578d1b730
Tom <tomplaysrbxonyt@gmail.com>
2026-08-08 19:16:25 +0200

    Test v2

--- assets/js/chat-widget.js
+++ assets/js/chat-widget.js
@@ -3,7 +3,7 @@
 // =========================================================
 // À REMPLIR une fois la fonction "chat" déployée sur Supabase (voir README) :
 const CHAT_ENDPOINT = 'https://bvmovojwwieytjhszkfl.supabase.co/functions/v1/chat';
-const SUPABASE_ANON_KEY = 'sb_publishable_LJw7_zIoFr5fbRPoiWcK5w_49tNDqZx';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2bW92b2p3d2lleXRqaHN6a2ZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxODI5NTUsImV4cCI6MjEwMTc1ODk1NX0.VjVLICZabfq7f2qwiV8RknHjODyjym0lT6viaHFBc7I';
 // =========================================================
 
 (function () {
